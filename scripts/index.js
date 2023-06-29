4b799b4fb9f58ed6e5615f654462a607d859b5d0
Владислав Харченко <120054604+VladisKharchenko@users.noreply.github.com>
2023-06-29 12:16:52 +0400

    Правки от 29.06.2023-2

--- scripts/index.js
+++ scripts/index.js
@@ -10,8 +10,10 @@ burgerButton.addEventListener('click', () => {
   if (burgerMenu.classList.contains('open')) {
     burgerButtonImage.src = './images/Cross.png';
     header.classList.add('shadow');
+    burgerButton.classList.add('no-hover');
   } else {
     burgerButtonImage.src = './images/Burger.png';
     header.classList.remove('shadow');
+    burgerButton.classList.remove('no-hover');
   }
 });
